9e72f647b3f9efe7d44f06b7147adad1fb72d102
Miguel Angel Ruiz <miguelruiz2205@gmail.com>
2023-05-25 12:10:32 -0500

    Acudiente service

---
+++ src/main/java/com/example/colegioelite/servicio/AcudienteServicio.java
@@ -0,0 +1,79 @@
+package com.example.colegioelite.servicio;
+
+import com.example.colegioelite.entidades.Acudiente;
+import com.example.colegioelite.repositorio.AcudienteRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AcudienteServicio  implements ServicioBase<Acudiente>{
+    @Autowired
+    protected AcudienteRepositorio acudienteRepositorio;
+
+    @Override
+    public List<Acudiente> buscarTodos() throws Exception {
+        try {
+            return acudienteRepositorio.findAll();
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public Acudiente buscarPorId(Integer id) throws Exception {
+        try {
+            Optional<Acudiente> acudienteOpcional = acudienteRepositorio.findById(id);
+            if (acudienteOpcional.isPresent()) {
+                return acudienteOpcional.get();
+            } else {
+                throw new Exception("Usuario no encontrado");
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public Acudiente registrar(Acudiente datosARegistrar) throws Exception {
+        try {
+            return acudienteRepositorio.save(datosARegistrar);
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public Acudiente actualizar(Integer id, Acudiente datosNuevos) throws Exception {
+        try {
+            Optional<Acudiente> acudienteOpcional = acudienteRepositorio.findById(id);
+            if (acudienteOpcional.isPresent()) {
+                Acudiente acudienteExistente = acudienteOpcional.get();
+                acudienteExistente.setNombre(datosNuevos.getNombre());
+                acudienteExistente.setTelefono(datosNuevos.getTelefono());
+                return acudienteRepositorio.save(acudienteExistente);
+            } else {
+                throw new Exception("Usuario no encontrado");
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public Boolean eliminar(Integer id) throws Exception {
+        try {
+            Optional<Acudiente> acudienteOpcional = acudienteRepositorio.findById(id);
+            if (acudienteOpcional.isPresent()) {
+                acudienteRepositorio.deleteById(id);
+                return true;
+            } else {
+                throw new Exception("Usuario no encontrado");
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+}
